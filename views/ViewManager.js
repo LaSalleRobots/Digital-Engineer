f62d5c3802d7af248f5a7eb4dd44a4d68e3adf48
Lukas Werner <lwerner@lshigh.org>
2021-01-02 21:31:51 -0800

    Start heading to v1 release

---
+++ views/ViewManager.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+//import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import * as NewNote from './NewNote';
+import * as DrawingTest from './DrawingTest';
+import * as NoteCardView from './NoteCardView';
+import {CameraTester} from './CameraView';
+import {SearchView} from './SearchView';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {QueryClient, QueryClientProvider, useQueryClient} from 'react-query';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer theme={DefaultTheme}>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: '#83b2d0',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen
+            name="Notes"
+            component={SearchView}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Icon
+                  name={'document-text-outline'}
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="New"
+            component={NewNote.App}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Icon name={'add'} color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
